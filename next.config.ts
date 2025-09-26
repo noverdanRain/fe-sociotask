e19bf05301c12e18da789e5a1ca8c62a77bcd1d1
Galang Arsandy <galangarsandy@gmail.com>
2025-09-26 18:07:06 +0700

    feat: update Next.js configuration and add some assets
    - Added webpack externals for "pino-pretty", "lokijs", and "encoding" in next.config.ts.
    - Included @chakra-ui/cli as a dev dependency in package.json.
    - Added new coin icons (AVAX, ETH, USDC, USDT) in both PNG and SVG formats.
    - Introduced highlight-scribbles.svg asset for landing page.
    - Updated tsconfig.json to include type definitions from the types directory.

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    webpack: (config) => {
+        config.externals.push("pino-pretty", "lokijs", "encoding");
+        return config;
+    },
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
     },
 };
-
 const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
